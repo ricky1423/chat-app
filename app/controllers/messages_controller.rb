b7a38df980c4c9ca46f5afdf50f5bad21a4d50bc
ricky1423 <rikisoccer0904@gmail.com>
2024-04-24 18:26:38 +0900

    空のメッセージが送信出来ないバリテーションの設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
-    @messages = @room.messages.include(:user)
+    @messages = @room.messages.includes(:user)
   end
 
   def create
@@ -11,7 +11,7 @@ class MessagesController < ApplicationController
     if @message.save
       ridirect_to room_messages_path(@room)
     else
-      @messages = @room.messages.include(:user)
+      @messages = @room.messages.includes(:user)
       render :index, status: :unprocessable_entity
     end
   end
